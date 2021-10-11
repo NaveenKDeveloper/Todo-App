ebf6400b07a1cb1b3a038de1254d64e3c0351f7c
Naveen K <naveen8972@gmail.com>
2021-10-12 00:28:48 +0530

    adding icons

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,7 +3,7 @@ import Form from "./Form";
 import { RiCloseCircleLine } from 'react-icons/ri';
 import { TiEdit } from 'react-icons/ti';
 
-function Todo() {
+function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
   const [edit, setEdit] = useState({
     id: null,
     value: "",
