dede6fbb850ff046f41fe3eb3fbeae25cc5c9e8c
Naveen K <naveen8972@gmail.com>
2021-10-12 00:36:15 +0530

    adding CRUD

--- src/components/List.js
+++ src/components/List.js
@@ -1,5 +1,6 @@
 import React,{useState} from 'react'
 import Form from './Form'
+import Todo from './Todo';
 
 function List() {
 
@@ -15,10 +16,39 @@ function List() {
         setTodos(newTodos)
         
     }
+    const updateTodo = (todoId, newValue) => {
+        if (!newValue.text || /^\s*$/.test(newValue.text)) {
+          return;
+        }
+    
+        setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
+      };
+
+      
+    const removeTodo = id => {
+        const removedArr = [...todos].filter(todo => todo.id !== id);
+    
+        setTodos(removedArr);
+      };
+    const completeTodo = id => {
+        let updatedTodos = todos.map(todo => {
+          if (todo.id === id) {
+            todo.isComplete = !todo.isComplete;
+          }
+          return todo;
+        });
+        setTodos(updatedTodos);
+      };
     return (
         <div>
             <h1>whats the plan</h1>
-           <Form onSubmit={addTodo}/> 
+            <Form onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      /> 
         </div>
     )
 }
